c7a9cf2cd03a4393972f4dab01edbba5f75c9ce1
Rafael Torok <rafaeltorok@gmail.com>
2025-01-28 16:07:17 -0300

    The MongoDB scripts are in a separate folder now Removed unnecessary dependencies

---
+++ notes/backend/mongodb/findNotes.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+  `mongodb+srv://rafael:${password}@notes0.u7crq.mongodb.net/?retryWrites=true&w=majority&appName=Notes0`
+
+mongoose.set('strictQuery',false)
+
+mongoose.connect(url)
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+Note.find({}).then(result => {
+  result.forEach(note => {
+    console.log(note)
+  })
+  mongoose.connection.close()
+})
