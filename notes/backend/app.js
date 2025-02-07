b9377e5d82a50793cbd0ebc60fde569341c2b6c4
Rafael Torok <rafaeltorok@gmail.com>
2025-02-07 17:53:55 -0300

    Added token authorization logins to add new notes

--- notes/backend/app.js
+++ notes/backend/app.js
@@ -1,13 +1,14 @@
+require('express-async-errors')
 const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const cors = require('cors')
 const notesRouter = require('./controllers/notes')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
-require('express-async-errors')
 
 mongoose.set('strictQuery', false)
 
@@ -28,6 +29,7 @@ app.use(middleware.requestLogger)
 
 app.use('/api/notes', notesRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
