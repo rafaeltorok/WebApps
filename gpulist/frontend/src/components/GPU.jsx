f7cf46a768dde9cb03591e052c3fd9624e604c92
Rafael Torok <rafaeltorok@gmail.com>
2025-02-17 19:21:15 -0300

    Minor improvements Added a proper indentation to both files of 2 spaces Fixed the name of the scrollToGpu function on the GPU component

--- gpulist/frontend/src/components/GPU.jsx
+++ gpulist/frontend/src/components/GPU.jsx
@@ -4,110 +4,110 @@ import calculatePerformance from '../calculatePerformance.js';
 
 
 const GPU = forwardRef(({ gpu, onDelete, showAll }, ref) => {
-    const gpuPerformance = calculatePerformance(gpu);
-    const [showBody, setShowBody] = useState(false);
+	const gpuPerformance = calculatePerformance(gpu);
+	const [showBody, setShowBody] = useState(false);
 
-    // Sync individual state with global "Show All" toggle
-    useEffect(() => {
-        setShowBody(showAll);
-    }, [showAll])
+	// Sync individual state with global "Show All" toggle
+	useEffect(() => {
+		setShowBody(showAll);
+	}, [showAll])
 
-    return (
-        <table 
-            className='gpu-data-table'
-            ref={ref} // Forward the ref to the table element
-        >
-            <thead>
-                <tr>
-                    <th className={gpu.manufacturer.toLowerCase() === 'nvidia'
-                    ? 'nvidia-model-header'
-                    : gpu.manufacturer.toLowerCase() === 'amd'
-                    ? 'amd-model-header'
-                    : gpu.manufacturer.toLowerCase() === 'intel'
-                    ? 'intel-model-header'
-                    : 'model-header'}
-                    colSpan={2}>
-                        {gpu.manufacturer} {gpu.gpuline} {gpu.model}
-                    </th>
-                </tr>
-                <tr>
-                    <th colSpan={2} className='table-header'>
-                        <button
-                            onClick={() => setShowBody(!showBody)}
-                        >
-                            {showBody ? "Hide" : "Show"}
-                        </button>
-                    </th>
-                </tr>
-            </thead>
-            {showBody && (
-                <tbody>
-                    <tr>
-                        <th className="table-header" colSpan={2}>SPECIFICATIONS</th>
-                    </tr>
-                    <tr>
-                        <th>CORES</th>
-                        <td>{gpu.cores}</td>
-                    </tr>
-                    <tr>
-                        <th>TMUs</th>
-                        <td>{gpu.tmus}</td>
-                    </tr>
-                    <tr>
-                        <th>ROPs</th>
-                        <td>{gpu.rops}</td>
-                    </tr>
-                    <tr>
-                        <th>VRAM</th>
-                        <td>{gpu.vram}GB {gpu.memtype}</td>
-                    </tr>
-                    <tr>
-                        <th>BUS WIDTH</th>
-                        <td>{gpu.bus} bit</td>
-                    </tr>
-                    <tr>
-                        <th className="table-header" colSpan={2}>CLOCK SPEEDS</th>
-                    </tr>
-                    <tr>
-                        <th>BASE CLOCK</th>
-                        <td>{gpu.baseclock} MHz</td>
-                    </tr>
-                    <tr>
-                        <th>BOOST CLOCK</th>
-                        <td>{gpu.boostclock} MHz</td>
-                    </tr>
-                    <tr>
-                        <th>MEMORY CLOCK</th>
-                        <td>{gpu.memclock} Gbps effective</td>
-                    </tr>
-                    <tr>
-                        <th className="table-header" colSpan={2}>THEORETICAL PERFORMANCE</th>
-                    </tr>
-                    <tr>
-                        <th>FP32(float)</th>
-                        <td>{gpuPerformance[0]}</td>
-                    </tr>
-                    <tr>
-                        <th>TEXTURE RATE</th>
-                        <td>{gpuPerformance[1]}</td>
-                    </tr>
-                    <tr>
-                        <th>PIXEL RATE</th>
-                        <td>{gpuPerformance[2]}</td>
-                    </tr>
-                    <tr>
-                        <th>BANDWIDTH</th>
-                        <td>{gpuPerformance[3]}</td>
-                    </tr>
-                    <tr>
-                        <td colSpan={"2"} id='delete-gpu-button'>
-                            <button onClick={() => onDelete(gpu.id, gpu.manufacturer, gpu.gpuline, gpu.model)}>Delete</button>
-                        </td>
-                    </tr>
-                </tbody>
-            )}
-        </table>
-    );
+	return (
+		<table 
+			className='gpu-data-table'
+			ref={ref} // Forward the ref to the table element
+		>
+			<thead>
+				<tr>
+					<th className={gpu.manufacturer.toLowerCase() === 'nvidia'
+						? 'nvidia-model-header'
+						: gpu.manufacturer.toLowerCase() === 'amd'
+						? 'amd-model-header'
+						: gpu.manufacturer.toLowerCase() === 'intel'
+						? 'intel-model-header'
+						: 'model-header'}
+						colSpan={2}>
+						{gpu.manufacturer} {gpu.gpuline} {gpu.model}
+					</th>
+				</tr>
+				<tr>
+					<th colSpan={2} className='table-header'>
+						<button
+							onClick={() => setShowBody(!showBody)}
+						>
+							{showBody ? "Hide" : "Show"}
+						</button>
+					</th>
+				</tr>
+			</thead>
+			{showBody && (
+				<tbody>
+					<tr>
+						<th className="table-header" colSpan={2}>SPECIFICATIONS</th>
+					</tr>
+					<tr>
+						<th>CORES</th>
+						<td>{gpu.cores}</td>
+					</tr>
+					<tr>
+						<th>TMUs</th>
+						<td>{gpu.tmus}</td>
+					</tr>
+					<tr>
+						<th>ROPs</th>
+						<td>{gpu.rops}</td>
+					</tr>
+					<tr>
+						<th>VRAM</th>
+						<td>{gpu.vram}GB {gpu.memtype}</td>
+					</tr>
+					<tr>
+						<th>BUS WIDTH</th>
+						<td>{gpu.bus} bit</td>
+					</tr>
+					<tr>
+						<th className="table-header" colSpan={2}>CLOCK SPEEDS</th>
+					</tr>
+					<tr>
+						<th>BASE CLOCK</th>
+						<td>{gpu.baseclock} MHz</td>
+					</tr>
+					<tr>
+						<th>BOOST CLOCK</th>
+						<td>{gpu.boostclock} MHz</td>
+					</tr>
+					<tr>
+						<th>MEMORY CLOCK</th>
+						<td>{gpu.memclock} Gbps effective</td>
+					</tr>
+					<tr>
+						<th className="table-header" colSpan={2}>THEORETICAL PERFORMANCE</th>
+					</tr>
+					<tr>
+						<th>FP32(float)</th>
+						<td>{gpuPerformance[0]}</td>
+					</tr>
+					<tr>
+						<th>TEXTURE RATE</th>
+						<td>{gpuPerformance[1]}</td>
+					</tr>
+					<tr>
+						<th>PIXEL RATE</th>
+						<td>{gpuPerformance[2]}</td>
+					</tr>
+					<tr>
+						<th>BANDWIDTH</th>
+						<td>{gpuPerformance[3]}</td>
+					</tr>
+					<tr>
+						<td colSpan={"2"} id='delete-gpu-button'>
+							<button onClick={() => onDelete(gpu.id, gpu.manufacturer, gpu.gpuline, gpu.model)}>Delete</button>
+						</td>
+					</tr>
+				</tbody>
+			)}
+		</table>
+	);
 })
 
 GPU.displayName = "GPU"
--- gpulist/frontend/src/components/PageIndex.jsx
+++ gpulist/frontend/src/components/PageIndex.jsx
@@ -10,53 +10,53 @@ const PageIndex = forwardRef(({ gpusData, gpuRefs }, ref) => {
   }))
 
   // Scroll to gpu when index item is clicked
-  const scrollTogpu = (id) => {
+  const scrollToGpu = (id) => {
     gpuRefs.current[id]?.scrollIntoView({ behavior: 'smooth', block: 'start' })
   }
   
   return (
     <div id="page-index">      
-        <h2 className='page-index-title'>
-            <button
-                id='show-index-button'
-                type='button'
-                onClick={() => setShowIndex((prev) => !prev)}
-            >
-                {showIndex ? "Hide index" : "Show index"}
-            </button>
-        </h2>
-        {showIndex && (
-            <ul className="page-index-list">
-                {gpusData.map((gpu) => (
-                <li key={gpu.id}>
-                    <button 
-                        className='index-item-button'
-                        onClick={() => scrollTogpu(gpu.id)} 
-                    >
-                    <span
-                        className={gpu.manufacturer.toLowerCase() === 'nvidia'
-                            ? 'nvidia-model-header'
-                            : gpu.manufacturer.toLowerCase() === 'amd'
-                            ? 'amd-model-header'
-                            : gpu.manufacturer.toLowerCase() === 'intel'
-                            ? 'intel-model-header'
-                            : 'model-header'}
-                    >{gpu.manufacturer} {gpu.gpuline} {gpu.model}
-                    </span>
-                    </button>
-                </li>
-                ))}
-            </ul>
-        )}
-    </div>
+			<h2 className='page-index-title'>
+				<button
+					id='show-index-button'
+					type='button'
+					onClick={() => setShowIndex((prev) => !prev)}
+				>
+					{showIndex ? "Hide index" : "Show index"}
+				</button>
+			</h2>
+			{showIndex && (
+				<ul className="page-index-list">
+					{gpusData.map((gpu) => (
+						<li key={gpu.id}>
+							<button 
+								className='index-item-button'
+								onClick={() => scrollToGpu(gpu.id)} 
+							>
+								<span
+									className={gpu.manufacturer.toLowerCase() === 'nvidia'
+										? 'nvidia-model-header'
+										: gpu.manufacturer.toLowerCase() === 'amd'
+										? 'amd-model-header'
+										: gpu.manufacturer.toLowerCase() === 'intel'
+										? 'intel-model-header'
+										: 'model-header'}
+									>{gpu.manufacturer} {gpu.gpuline} {gpu.model}
+								</span>
+							</button>
+						</li>
+					))}
+				</ul>
+			)}
+		</div>
   )
 })
 
 PageIndex.displayName = "PageIndex"
 
 PageIndex.propTypes = {
-    gpusData: PropTypes.array.isRequired,
-    gpuRefs: PropTypes.object.isRequired
+	gpusData: PropTypes.array.isRequired,
+	gpuRefs: PropTypes.object.isRequired
 }
 
 export default PageIndex
