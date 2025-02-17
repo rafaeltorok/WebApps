770934d0d3e2e4fb4cb954518d3d99226ff980a1
Rafael Torok <rafaeltorok@gmail.com>
2025-02-17 17:45:16 -0300

    Changed the router name on the testing controller

--- gpulist/backend/controllers/testing.js
+++ gpulist/backend/controllers/testing.js
@@ -1,10 +1,10 @@
-const gpuRouter = require('express').Router()
+const testingRouter = require('express').Router()
 const Gpu = require('../models/gpu.js')
 
-gpuRouter.post('/reset', async (request, response) => {
+testingRouter.post('/reset', async (request, response) => {
   await Gpu.deleteMany({})
 
   response.status(204).end()
 })
 
-module.exports = gpuRouter
+module.exports = testingRouter
