0312551513569d1d2054af66b21398a1f6404391
Rafael Torok <rafaeltorok@gmail.com>
2025-06-13 18:03:20 -0300

    Implemented server support for running files from the dist folder Added the client production build to the server folder

--- user-management-app/server/src/index.js
+++ user-management-app/server/src/index.js
@@ -2,28 +2,32 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
-dotenv.config();  // Load vars from .env
+const path = require('path');
+dotenv.config();
 
 const app = express();
 app.use(cors());
-app.use(express.json());  // Parse JSON bodies
+app.use(express.json());
 
-// Connect tp MongoDB (Mongoose)
-// We use mongoose.connect() with our URI from .env
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('MongoDB connected'))
   .catch((err) => console.error('MongoDB failed to connect:', err));
 
-// Import routes
 const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/users');
 
-// Use routes
 app.use('/api/auth', authRoutes);
-app.use('/api/users', userRoutes)
+app.use('/api/users', userRoutes);
 
-// Routes
-// start server
+// Serve static files from the /dist folder inside server
+const distPath = path.join(__dirname, '../dist');
+app.use(express.static(distPath));
+
+// Catch-all for non-API routes
+app.get('/{*any}', (req, res) => {
+  res.sendFile(path.join(distPath, 'index.html'));
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`);
